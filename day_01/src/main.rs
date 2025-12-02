9219edccce9c242602fe4abc94b7836dda327f7c
devkav <devankav7@gmail.com>
2025-12-01 23:58:31 -0500

    Completed day 1

---
+++ day_01/src/main.rs
@@ -0,0 +1,44 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+const DIAL_SIZE: i16 = 100;
+
+fn main() {
+    let file = File::open("./src/input.txt").expect("File was unable to be opened");
+    let reader = BufReader::new(file);
+    let mut dial: i16 = 50;
+    let mut count: i32 = 0;
+
+    for line in reader.lines() {
+        let line = line.expect("Should be able to read line");
+        let line = line.trim();
+
+        let direction = line.chars().nth(0).unwrap();
+        let num: i16 = (&line[1..]).parse().expect("Failed to parse string to i16");
+        let sign: i16;
+
+        if direction == 'L' {sign = -1;} else {sign = 1;}
+        let num = sign * num;
+        let was_positive = dial > 0;
+
+        dial += num;
+
+        count += (dial / DIAL_SIZE).abs() as i32;
+        if (dial < 0 && was_positive) || dial == 0 {count += 1;}
+
+        dial %= DIAL_SIZE;
+
+        if dial < 0 {
+            dial += DIAL_SIZE;
+        }
+
+        println!("{}, dial: {}, num {}, passes: {}", line, dial, num, count);
+
+
+
+        //println!("dir: {}, num: {}, dial: {}, num_turns: {}, count: {}", direction, num, dial, -1, count);
+    }
+
+    println!("{}", count);
+}
