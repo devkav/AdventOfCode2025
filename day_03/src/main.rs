547ec5f9f74a5db54f158454453c0642460b004d
DevanTurtle7 <devankav7@gmail.com>
2025-12-16 22:55:07 -0500

    Part 2 done

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,10 +1,10 @@
 mod part1;
-// mod part2;
+mod part2;
 
 fn main() {
     let part1_result = part1::run();
-    // let part2_result = part2::run();
+    let part2_result = part2::run();
 
     println!("Part 1: {}", part1_result);
-    // println!("Part 2: {}", part2_result);
+     println!("Part 2: {}", part2_result);
 }
