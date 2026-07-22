bdeec25f49b52fa3c2c58087ed49bf373543fbd3
devkav <devankav7@gmail.com>
2026-07-21 21:41:57 -0400

    Day 5 part 2

--- days/src/bin/day05/part2.rs
+++ days/src/bin/day05/part2.rs
@@ -1,6 +1,43 @@
+use core::fmt;
+use std::cmp::{min, max};
+
+#[derive(Clone, Copy)]
+struct Range {
+    start: usize,
+    end: usize
+}
+
+impl Range {
+    fn can_merge(&self, other: &Range) -> bool {
+        if (self.start <= other.end && self.end >= other.start) || 
+            (self.start >= other.end && self.end <= other.start) ||
+            (self.start == other.end + 1) ||
+            (self.end == other.start + 1) {
+                return true;
+        }
+
+        return false;
+    }
+
+    fn merge(&mut self, other: &Range) {
+        self.start = min(self.start, other.start);
+        self.end = max(self.end, other.end);
+    }
+
+    fn size(&self) -> usize {
+        (self.end - self.start) + 1
+    }
+}
+
+impl fmt::Display for Range {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}->{}", self.start, self.end)
+    }
+}
+
 pub fn run(input: &str) -> usize {
     let mut count = 0;
-    let mut ranges: Vec<(usize, usize)> = Vec::new();
+    let mut ranges: Vec<Range> = Vec::new();
     let mut planning = true;
 
     for line in input.lines() {
@@ -14,33 +51,34 @@ pub fn run(input: &str) -> usize {
         if !planning {continue}
 
         if planning {
-            let tokens: Vec<&str> = line.split("-").collect();
-            if tokens.len() != 2 {panic!("Expected 2 tokens, got: {}", tokens.len())}
+            let (start_str, end_str) = line.split_once("-").unwrap();
 
-            let start: usize = tokens[0].parse().expect("Unable to parse token");
-            let end: usize = tokens[1].parse().expect("Unable to parse token");
-            let mut skip = false;
+            ranges.push(Range {
+                start: start_str.parse().unwrap(),
+                end: end_str.parse().unwrap()
+            });
+            ranges.sort_by_key(|r| r.start);
 
-            for range in &ranges {
-                let (current_start, current_end) = range;
+            let mut i = 0;
 
-                if start >= *current_start && end <= *current_end {
-                    // No ranges left over
-                    skip = true;
-                    break;
-                } else if start < *current_start && end > *current_start {
-                    // Creates two ranges
-                } else if start < *current_start && end <= *current_end {
-                    // First half of range left over
-                } else if start >= *current_start && end > *current_end {
-                    // Second half of range is left over
+            while i < ranges.len() - 1 {
+                let range = ranges[i];
+                let next_range = ranges[i+1];
+
+                if range.can_merge(&next_range) {
+                    ranges[i].merge(&next_range);
+                    ranges.remove(i + 1);
+                    i = 0;
+                    continue;
                 }
+
+                i += 1;
             }
-
-            if skip {continue}
-            //ranges.push((start, end));
-
         }
+    }
+
+    for range in ranges {
+        count += range.size();
     }
 
    return count;
