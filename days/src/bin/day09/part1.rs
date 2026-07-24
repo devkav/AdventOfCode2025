a03a26c76b1f2dc51841f3d0770b061c8a454cd4
DevanTurtle7 <devankav7@gmail.com>
2026-07-23 22:43:38 -0400

    Starting day 9

--- days/src/bin/day09/part1.rs
+++ days/src/bin/day09/part1.rs
@@ -1,3 +1,21 @@
-pub fn run(_input: &str) -> usize {
+struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+pub fn run(input: &str) -> usize {
+    let mut tiles: Vec<Coordinate> = Vec::new();
+
+    for line in input.lines() {
+        let nums: Vec<usize> = line.trim()
+            .split(",")
+            .map(|num_str| num_str.parse::<usize>().unwrap())
+            .collect();
+        let x = nums[0];
+        let y = nums[1];
+
+        tiles.push(Coordinate { x, y });
+    }
+
     return 0;
 }
